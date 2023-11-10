71da06a37d9a00dd6fc4a46f376dd54e7ccdefe7
arekebyu <jonathancyw@gmail.com>
2023-11-10 12:19:12 -0800

    I have no clue what I am doing or where I'm going

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"; 
+
+function Header({ title }) {
+    return <h1></h1>
+}
+
+export default function HomePage() {
+    return(
+        <div>
+        </div>
+    )
+}
